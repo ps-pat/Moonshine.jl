7932a49ae968385a26533dbbabdff131c7bba2b8
Patrick Fournier <p_fournier@hushmail.com>
2023-08-28 22:29:52 -0400

    Optimize Khatri-Rao product.

--- src/KhatriRaoProd.jl
+++ src/KhatriRaoProd.jl
@@ -1,15 +1,21 @@
-function ⊙(A::AbstractMatrix{T}, B::AbstractMatrix{T}) where T
-    r = size(A, 2)
-    r == size(B, 2) ||
+Base.@assume_effects :inaccessiblememonly :terminates_globally :effect_free :notaskstate function ⊙(A::AbstractMatrix{T}, B::AbstractMatrix{T}) where T
+    c = size(A, 2)
+    c == size(B, 2) ||
         throw(DimensionMismatch(lazy"matrix A has dimension $(size(A)), " *
                                     "matrix B has dimension $(size(B))"))
 
     p, q = size(A, 1), size(B, 1)
 
-    ret = similar(A, T, (p * q, r))
+    ret = similar(A, T, (p * q, c))
 
-    @inbounds for (k, (α, β)) ∈ (enumerate ∘ zip)(eachcol(A), eachcol(B))
-        ret[:,k] .= kron(α, β)
+    k = 1
+    for j ∈ 1:c
+        for iA ∈ 1:p
+            @simd for iB ∈ 1:q
+                @inbounds ret[k] = A[iA, j] * B[iB, j]
+                k += 1
+            end
+        end
     end
 
     ret
