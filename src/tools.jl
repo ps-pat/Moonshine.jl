3a96ddcbb921329192b1376e678bb6028f939006
Patrick Fournier <p_fournier@hushmail.com>
2023-11-09 12:55:53 -0500

    Moove Sets of intervals stuff.

--- src/tools.jl
+++ src/tools.jl
@@ -1,3 +1,14 @@
+using IntervalSets: AbstractInterval, Interval
+
+import Base:
+    union,
+    intersect,
+    join
+
+######################
+# Khatri-Rao Product #
+######################
+
 Base.@assume_effects :inaccessiblememonly :terminates_globally :effect_free :notaskstate function ⊙(A::AbstractMatrix{T}, B::AbstractMatrix{T}) where T
     c = size(A, 2)
     c == size(B, 2) ||
@@ -20,3 +31,59 @@ Base.@assume_effects :inaccessiblememonly :terminates_globally :effect_free :not
 
     ret
 end
+
+####################
+# Set of intervals #
+####################
+
+function simplify!(xs)
+    tmp = Set{eltype(xs)}()
+
+    while !isempty(xs)
+        @label main
+        x = pop!(xs)
+        isempty(x) && continue
+
+        for y ∈ xs
+            isempty(x ∩ y) && continue
+            push!(xs, x ∪ pop!(xs, y))
+            @goto main
+        end
+
+        push!(tmp, x)
+    end
+
+    while !isempty(tmp)
+        push!(xs, pop!(tmp))
+    end
+
+    xs
+end
+
+union(x::T, xs::Set{T}) where T =
+    simplify!(Set([x]) ∪ xs)
+
+function union(x::Set{T}, y::Set{T}) where T<:Interval
+    ret = empty(x)
+    for el ∈ x
+        push!(ret, el)
+    end
+
+    for el ∈ y
+        push!(ret, el)
+    end
+
+    simplify!(ret)
+end
+
+intersect(x::T, xs::Set{T}) where T =
+    (simplify! ∘ Set ∘ broadcast)(Fix1(intersect, x), xs)
+
+intersect(xs::Set{T}, ys::Set{T}) where T =
+    (simplify! ∘ mapreduce)(x -> intersect(x, ys), ∪, xs)
+
+for fun ∈ [:union, :intersect]
+    @eval $fun(xs::Set{T}, x::T) where T = $fun(x, xs)
+end
+
+in(x::T, s::Set{<:AbstractInterval{T}}) where T = any(int -> x ∈ int, s)
