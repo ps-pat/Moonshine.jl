08789ec9434cdd13db692e38ce2e4c61d1e83532
Patrick Fournier <p_fournier@hushmail.com>
2025-04-18 16:48:09 -0400

    Doc: Improve `plot_breakpoint` documentation

--- src/Arg/Arg.jl
+++ src/Arg/Arg.jl
@@ -193,10 +193,16 @@ export plot_breakpoints
 """
     plot_breakpoints(arg; kwargs...)
 
-Heatmap of recombination events' positions. Additional keywords arguments are
-passed directly to [`UnicodePlots.histogram`](@ref).
+Heatmap of recombination events' positions.
 
 See also [`breakpoints`](@ref)
+
+# Keywords
+* `nbins` (`clamp(nrecombinations(arg) ÷ 100, 1, 69)`): number of bins. Its
+  default maximum value of 69 produces a nice 80 columns wide plot.
+* `height` (`7`): height of the plot.
+* `kwargs...`: additional keywords arguments are passed directly to
+  [`UnicodePlots.histogram`](@ref).
 """
 function plot_breakpoints(arg;
                           nbins = clamp(nrecombinations(arg) ÷ 100, 1, 69),
